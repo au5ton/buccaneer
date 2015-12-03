192effdd5281d6076cf30e32870899d372728e8e
Austin Jackson <au5ton@icloud.com>
2015-12-02 22:46:17 -0600

    Improved efficiency

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 var tpb = require('thepiratebay');
 var fw = require('./fw');
 
-var page = 0;
+var page = 4;
 var limit = 30;
+var postCount = 0;
+var postLimit = 20; //Daily limit imposed by Fluff
 function recursive() {
+    console.log('STARTING PAGE '+page);
     fw.authenticate(function() {
         tpb.search('', { category: '0', page: page, orderBy: '7' })
         .then(function(results){
             for(var i = 0; i < results.length; i++) {
-                fw.postTorrent(results[i], function(){
-                    console.log('done');
-                });
+                if(postCount < postLimit) {
+                    fw.postTorrent(results[i], function(){
+                        console.log('done');
+                    });
+                    postCount++;
+                }
+                //console.log(fw.categoryMatcher(results[i]));
             }
 
             if(page < limit) {
