37ac14ea6de3279484c4a4009bd71e759b258047
Austin Jackson <au5ton@icloud.com>
2016-02-25 01:21:57 -0600

    update

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var page = 0; //TVD
 function recursive_torrents() {
     console.log('STARTING PAGE '+page);
     fw.authenticate(function() {
-        tpb.search('The Big Bang Theory', {
+        tpb.search('', {
             category: '0',
             page: page,
             orderBy: '7'
@@ -38,6 +38,30 @@ function recursive_toptorrents() {
                     });
                 //console.log(fw.categoryMatcher(results[i]));
             }
+        }).catch(function(err){
+            console.log(err);
+        });
+    });
+}
+
+function recursive_best_torrents() {
+    console.log('STARTING PAGE '+page);
+    fw.authenticate(function() {
+        tpb.search('', {
+            category: '0',
+            page: page,
+            orderBy: '7'
+        })
+        .then(function(results){
+            for(var i = 0; i < results.length; i++) {
+                if(results[i]['seeders'] > 0) {
+                    fw.postTorrent(results[i], function(){
+                        console.log('done');
+                    });
+                }
+            }
+            page++;
+            recursive_best_torrents();
         }).catch(function(err){
             console.log(err);
         });
@@ -87,9 +111,57 @@ else if(process.argv[2] === '--spamTorrents') {
 else if(process.argv[2] === '--spamTopTorrents') {
     recursive_toptorrents();
 }
+else if(process.argv[2] === '--spamBestTorrents') {
+    recursive_best_torrents();
+}
+else if(process.argv[2] === '--search') {
+    var category = '0';
+    var query = '';
+    for(var i = 0; i < process.argv.length; i++) {
+        if(process.argv[i] === '-cat') {
+            category = process.argv[i+1];
+        }
+        if(process.argv[i] === '-query') {
+            query = process.argv.splice(i+1).join(' ');
+        }
+    }
+    
+    if(category === 'ALL_AUDIO') {
+        category = '100';
+    }
+    if(category === 'ALL_VIDEO') {
+        category = '200';
+    }
+    if(category === 'ALL_APPS') {
+        category = '300';
+    }
+    if(category === 'ALL_GAMES') {
+        category = '400';
+    }
+    if(category === 'ALL_PORN') {
+        category = '500';
+    }
+    
+    console.log('Searching TPB under category \''+category+'\' for: \''+query+'\'...');
+    
+    tpb.search(query, {
+        category: category,
+        orderBy: '7'
+    }).then(function(results){
+        //console.log(results);
+        for(var i = 0; i < results.length; i++) {
+            console.log(results[i]['name']);
+            console.log(results[i]['seeders']);
+            //console.log(results[i]['magnetLink']);
+            //console.log(fw.categoryMatcher(results[i]));
+        }
+    }).catch(function(err){
+        console.log(err);
+    });
+}
 else {
 
-    tpb.search('The Big Bang Theory', {
+    tpb.search('minecraft', {
         category: '0',
         orderBy: '7'
     }).then(function(results){
